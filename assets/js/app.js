3303840b2fd9e0a4f31f3a1661f60e74b1b5b059
Dion Rats <dionrats@Dions-MBP.mgmt.quintor.lan>
2019-04-26 11:06:37 +0200

    h

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,14 +3,14 @@ var deviceid;
 $('document').ready(function() {
     'use strict'
     initTimer();
-    initSpotify();
+    // initSpotify();
     
 
-    checkSpotifySession();
-    checkConnected();
+    // checkSpotifySession();
+    // checkConnected();
 
-    startbackgroundloop(30000);
-    startSpotifySessionloop(3000000);
+    // startbackgroundloop(30000);
+    // startSpotifySessionloop(3000000);
 });
 
 function initTimer() {
